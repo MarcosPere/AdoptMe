236712df8907954e2f8a6c9aa1e7503b3a04800e
Marcos Pereira <marcospere@gmail.com>
2021-01-20 14:35:52 -0300

    Fim do capitulo 3 (Parcel Bundle)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
@@ -12,15 +8,14 @@ const App = () => {
     React.createElement(Pet, {
       name: "Luna",
       animal: "Dog",
-      breed: "Havanese",
+      breed: "Havanese"
     }),
     React.createElement(Pet, {
       name: "Pepper",
       animal: "Bird",
-      breed: "Cockatiel",
+      breed: "Cockatiel"
     }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
   ]);
 };
-
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
